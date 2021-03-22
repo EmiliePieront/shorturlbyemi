64f3827bfc55c285dd60ec14add1eacdfe22a510
Emi <71385864+EmiliePieront@users.noreply.github.com>
2021-03-22 11:37:21 +0100

    Update index.php

--- index.php
+++ index.php
@@ -10,22 +10,22 @@
         <a href="http://mveuz.com" target="_blank"><img class="pubLogo"src="img/mvz1.png" alt="logo de Ma vie est un zapping"></a>
     </div>
 </section>
- ?>
-    <?php 
-        if(isset($_POST['user']{
-            $prenom = $_POST['user'];
-            //Prepare la requête
-            $req = $bdd->prepare('INSERT INTO users(username) VALUES(?)');
-            //Execute la requête
-            $req->execute(array($prenom));
-            echo $prenom;
-        }
-        
-    ?>
-    <form method="post" action="test.php">
-        <input name='user' type='text' placeholder="Enter your username">
-        <label for="user">What's your name?</label>
-    </form>
-    <button type="submit">Envoyer</button>
+
+<?php 
+    if(isset($_POST['user']{
+        $prenom = $_POST['user'];
+        //Prepare la requête
+        $req = $bdd->prepare('INSERT INTO users(username) VALUES(?)');
+        //Execute la requête
+        $req->execute(array($prenom));
+        echo $prenom;
+    }
+
+?>
+<form method="post" action="test.php">
+    <input name='user' type='text' placeholder="Enter your username">
+    <label for="user">What's your name?</label>
+</form>
+<button type="submit">Envoyer</button>
 
 <?php include "components/footer.php"?>
